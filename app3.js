d65df5a8598e2c41b84df5f4a471383e73226bb5
Merdan <merdanprog002@gmail.com>
2024-07-31 15:26:25 +0500

    added colorProjectInJS

---
+++ app3.js
@@ -0,0 +1,70 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+    if(event.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if(type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        :event.target.children[0]
+
+       node.classList.toggle('fa-lock-open')
+       node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClipboard(event.target.textContent)
+    }
+})
+
+
+function generateRandomColor() {
+    //RGB
+    //#FF0000
+    //#00FF00
+    //#0000FF
+
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for(let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function copyToClipboard(text) {
+   return navigator.clipboard.writeText(text)
+}
+
+
+function setRandomColors() {
+    cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random()
+
+        if(isLocked) {
+            return
+        }
+
+        text.textContent = color
+        col.style.background = generateRandomColor()
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
